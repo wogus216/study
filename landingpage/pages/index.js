a3337955051acc19a49cffb3b6c6490481f7c48d
wogus216 <wogus21694@gmail.com>
2022-03-15 18:19:16 +0900

    시작

---
+++ landingpage/pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head';
+import NavTabs from '../src/components/navbar';
+import { Container, Grid } from '@mui/material';
+import Header from '../src/components/navbar';
+
+export default function Home() {
+  return <div>알수 가 없네</div>;
+}
